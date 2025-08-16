4e7d3913f5c6465de29186049e2df46f29145b95
Riad <riad_mammadov@yahoo.com>
2025-08-16 15:14:06 +0100

    update main code to add db

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
-
+from schemas import Blog
+from models import Blog as BlogModel
+from database import engine
+from sqlmodel import SQLModel
 app = FastAPI()
 
+SQLModel.metadata.create_all(engine)
 
 @app.get('/')
 def index():
-    return 'Hello'
+    return {'data': 'blog list'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id:int):
+    return {'data': id}
+
+@app.post("/blog")
+def create_blog(req:Blog):
+    return {'data': f"Blog Created with {req.title}"}
+
+@app.get("/blog/{id}/comments")
+def comments(id:str):
+    return {'data': {'comments':'...'}}
