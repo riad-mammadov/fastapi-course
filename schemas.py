1bc0cb8158cb551689cbda87252b651dd53a685d
Riad <riad_mammadov@yahoo.com>
2025-08-17 00:00:21 +0100

    add user schema

--- schemas.py
+++ schemas.py
@@ -3,3 +3,14 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body: str
+
+class ShowBlog(BaseModel):
+    title: str
+    
+class User(BaseModel):
+    name: str
+    email: str
+    password:str
+
+class UserInfo(BaseModel):
+    name: str
