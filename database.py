86c367b8e84c41356058762f7b75854122229430
Riad <riad_mammadov@yahoo.com>
2025-08-17 00:01:58 +0100

    move session dep and engine to database file

--- database.py
+++ database.py
@@ -1,7 +1,15 @@
 from sqlmodel import create_engine
+from typing import Annotated
+from fastapi import Depends
+from sqlmodel import Session
 
 db = 'sqlite:///./blog.db'
 engine = create_engine(db, connect_args={"check_same_thread": False})
 
 
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
 
