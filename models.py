c814c5ea4c3f6fef8bd7fca39d174f2273694027
Riad <riad_mammadov@yahoo.com>
2025-08-17 04:21:54 +0100

    create foreign key & relationship between blog and user

--- models.py
+++ models.py
@@ -4,9 +4,11 @@ class Blog(SQLModel, table=True):
     id: int = Field(primary_key=True, index=True)
     title: str = Field(index = True)
     body: str
+    author_id: str = Field(default=None, foreign_key="user.id")
 
 class User(SQLModel, table = True):
     id: int = Field(primary_key=True, index=True)
     name: str = Field(index = True)
     email: str
     password: str
+
