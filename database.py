1923c3a632c6ef76ba665fdb9ae01b466790cf16
Riad <riad_mammadov@yahoo.com>
2025-08-17 23:00:34 +0100

    add session to file

--- database.py
+++ database.py
@@ -6,7 +6,6 @@ from sqlmodel import Session
 db = 'sqlite:///./blog.db'
 engine = create_engine(db, connect_args={"check_same_thread": False})
 
-
 def get_session():
     with Session(engine) as session:
         yield session
